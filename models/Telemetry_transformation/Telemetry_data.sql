74608b995591631679dcf4814c1148577a5f6e39
cdpuser <rahul.gajula@hcl.com>
2021-10-28 03:14:17 +0000

    final

--- models/Telemetry_transformation/Telemetry_data.sql
+++ models/Telemetry_transformation/Telemetry_data.sql
@@ -3,6 +3,3 @@ SELECT device_id, temperature, uv, volt, pressure, humidity, vibration
 FROM DEVICE_TELEMETRY_DATA
 where device_id between 'Device_2' and 'Device_5'
 order by temperature>=50
-
-
-    
